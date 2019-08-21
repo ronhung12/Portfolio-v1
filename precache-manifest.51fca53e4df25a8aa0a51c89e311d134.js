9051f77d7f475a6feda1228bfb57b892d50fbb12
Ronald Hung <rhung@fordham.edu>
2019-08-21 17:03:00 -0400

    Updates

---
+++ precache-manifest.51fca53e4df25a8aa0a51c89e311d134.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e659688147cec0fe6d4037db34378616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033c9a691e89ab202350",
+    "url": "/static/css/main.31058505.chunk.css"
+  },
+  {
+    "revision": "07efb644317a77d19637",
+    "url": "/static/js/2.810608e3.chunk.js"
+  },
+  {
+    "revision": "033c9a691e89ab202350",
+    "url": "/static/js/main.8826dae7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88733c3880bf544c7a4184e985fa23c1",
+    "url": "/static/media/basketball.88733c38.jpg"
+  },
+  {
+    "revision": "1c0cc4183b24a07c4378b11343269534",
+    "url": "/static/media/fishing.1c0cc418.jpg"
+  },
+  {
+    "revision": "f0f24e60b0b5f4f99364ee27685674ed",
+    "url": "/static/media/graduation1.f0f24e60.jpg"
+  },
+  {
+    "revision": "745798d0340eea4636f1dfc1be349354",
+    "url": "/static/media/portfolio_headshot.745798d0.jpg"
+  },
+  {
+    "revision": "2c8fb4b8fb26ff1d59ba00e8983dcb76",
+    "url": "/static/media/snowboarding.2c8fb4b8.jpg"
+  }
+]);
